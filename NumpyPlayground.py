c1ec94ad1ec4768aed2a4c9900972778c48ccee7
goldin2008 <goldin20082011@gmail.com>
2015-09-05 17:17:00 -0500

    Update NumpyPlayground.py

--- NumpyPlayground.py
+++ NumpyPlayground.py
@@ -15,7 +15,7 @@ lists in Python, except that every thing inside an array must be of the
 same type, like int or float.
 '''
 # Change False to True to see Numpy arrays in action
-if False:
+if True:
     array = np.array([1, 4, 5, 8], float)
     print array
     print ""
@@ -27,7 +27,7 @@ You can index, slice, and manipulate a Numpy array much like you would with a
 a Python list.
 '''
 # Change False to True to see array indexing and slicing in action
-if False:
+if True:
     array = np.array([1, 4, 5, 8], float)
     print array
     print ""
@@ -39,7 +39,7 @@ if False:
     print array[1]
 
 # Change False to True to see Matrix indexing and slicing in action
-if False:
+if True:
     two_D_array = np.array([[1, 2, 3], [4, 5, 6]], float)
     print two_D_array
     print ""
@@ -53,7 +53,7 @@ if False:
 Here are some arithmetic operations that you can do with Numpy arrays
 '''
 # Change False to True to see Array arithmetics in action
-if False:
+if True:
     array_1 = np.array([1, 2, 3], float)
     array_2 = np.array([5, 2, 6], float)
     print array_1 + array_2
@@ -63,7 +63,7 @@ if False:
     print array_1 * array_2
 
 # Change False to True to see Matrix arithmetics in action
-if False:
+if True:
     array_1 = np.array([[1, 2], [3, 4]], float)
     array_2 = np.array([[5, 6], [7, 8]], float)
     print array_1 + array_2
@@ -79,7 +79,7 @@ mean and dot product.
 
 Both of these functions will be useful in later programming quizzes.
 '''
-if False:
+if True:
     array_1 = np.array([1, 2, 3], float)
     array_2 = np.array([[6], [7], [8]], float)
     print np.mean(array_1)
