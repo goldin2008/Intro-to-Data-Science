6ee1f3c228b7cb4c555711a4f43a38a146705c40
goldin2008 <goldin20082011@gmail.com>
2015-09-07 10:25:30 -0500

    Create API_Exercise.py

---
+++ Lesson2/API_Exercise.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+#if __name__ == '__main__'
+
+def api_get_request(url):
+    # In this exercise, you want to call the last.fm API to get a list of the
+    # top artists in Spain.
+    #
+    # Once you've done this, return the name of the number 1 top artist in Spain.
+    
+    return ... # return the top artist in Spain
+    #Provide a URL we should make an API call to.
+    url = ''
+    
+    #Make our API call using the requests library and load the results into a dict.
+    data = requests.get(url).text
+    data = json.loads(data)
+    
+    #Print out the name of the #1 top artist.
+    print data['topartists']['artist'][0]['name']
