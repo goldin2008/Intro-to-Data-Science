cccf55d3a2d12e44ec092cf8be7fa7906acc8f00
goldin2008 <goldin20082011@gmail.com>
2015-09-13 20:29:55 -0500

    Update aadhaar_generated_reducer.py

--- Lesson5/aadhaar_generated_reducer.py
+++ Lesson5/aadhaar_generated_reducer.py
@@ -26,18 +26,24 @@ def reducer():
     
     for line in sys.stdin:
         #your code here
+        #reducer function will consume the key value pairs emitted by mapper
+        #so create and array data for every single line which will essentially be length of two containing the 
+        #key and the value. Note that we split on the tab, which we input into the output of our mapper.
         data = line.strip().split("\t")
         
+        #If for some reason data is less than or greater than length two, we continue on.
+        #There's something wrong with this key value pair and we shouldn't process it.
         if len(data) != 2:
             continue
           
         this_key, count = data
         
+        #So if this is a new key, let's submit the final key value pair.
         if old_key and old_key != this_key:
             print "{0}\t{1}".format(old_key, aadhaar_generated)
-            aadhaar_generated = 0
+            aadhaar_generated = 0 #Set aadhaar_generated value = 0 for new key
         
-        old_key = this_key
+        old_key = this_key #Set this key as a old key
         aadhaar_generated += float(count)
         
         if old_key != None:
